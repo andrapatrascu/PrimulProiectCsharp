8fabafd7edad6a53ea5434f98c36fc2da7b34771
andrapatrascu <patrascuandra@yahoo.com>
2022-08-28 16:50:28 +0300

    First homework
    - done the first 4 exercises

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello");
 Console.WriteLine("Andra");
+
+int x = 5;
+int y = 101;
+int suma = x + y;
+string gigi = "ana are mere";
+Console.WriteLine("suma dintre {0} si {1} este {2} si totodata {3}", x, y, suma, gigi);
+
+double z = 5.4;
+double t = 10100.56;
+double a_doua_suma = z+t;
+Console.WriteLine(a_doua_suma);
+
+double c = 5.4;
+double d = 2.1;
+double impartire = c/d;
+Console.WriteLine("Impartirea dintre {0} si {1} este {2:0.000}", c, d,impartire);
+
+int p = -5 + 8 * 6;
+Console.WriteLine("Rezultat = {0}", p);
+
+
+x = -5;
+y = 8;
+int tx = 6;
+int rezultat = x + y * tx;
+Console.WriteLine(rezultat);
+
+
+int l = (55 + 9) % 9;
+Console.WriteLine("Rezultatul este = {0}", l);
+
+int h = 20 + -3 * 5 / 8; 
+Console.WriteLine("Rezultatul este = {0}", h);
+
+int gt = 5 + 15 / 3 * 2 - 8 % 3;
+Console.WriteLine("Rezultatul este = {0}", gt);
