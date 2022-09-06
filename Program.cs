a69e0ac31cd1db19267be7d127b901f49e557282
Ramona Zmole <rzmole@northernsafety.com>
2022-09-06 10:57:38 +0300

    change to var

--- Program.cs
+++ Program.cs
@@ -2,42 +2,42 @@
 Console.WriteLine("Hello");
 Console.WriteLine("Andra");
 
-int x = 5;      // Am declarat variabila de tip int si i-am asignat valoarea 5
-int y = 101;
-int suma = x + y;
-string gigi = "ana are mere";
+var x = 5;      // Am declarat variabila de tip int si i-am asignat valoarea 5
+var y = 101;
+var suma = x + y;
+var gigi = "ana are mere";
 Console.WriteLine("suma dintre {0} si {1} este {2} si totodata {3}", x, y, suma, gigi);
 
-double z = 5.4;
-double t = 10100.56;
-double a_doua_suma = z+t;
+var z = 5.4;
+var t = 10100.56;
+var a_doua_suma = z+t;
 Console.WriteLine(a_doua_suma);
 
-double c = 5.4;
-double d = 2.1;
-double impartire = c/d;
+var c = 5.4;
+var d = 2.1;
+var impartire = c/d;
 Console.WriteLine("Impartirea dintre {0} si {1} este {2:0.000}", c, d,impartire);
 
-int p = -5 + 8 * 6;     // Am declarat variabila de tip int p si am initializat-o cu o expresie
+var p = -5 + 8 * 6;     // Am declarat variabila de tip int p si am initializat-o cu o expresie
 Console.WriteLine("Rezultat = {0}", p); 
 
 
 x = -5;     // Am asignat valoarea -5 variabilei x
 y = 8;
-int tx = 6;
-int rezultat = x + y * tx;
+var tx = 6;
+var rezultat = x + y * tx;
 Console.WriteLine(rezultat);
 
 
-int l = (55 + 9) % 9;
+var l = (55 + 9) % 9;
 Console.WriteLine("Rezultatul este = {0}", l);      // Afisez valoarea variabilei l
 
-int h = 20 + -3 * 5 / 8; 
+var h = 20 + -3 * 5 / 8; 
 Console.WriteLine("Rezultatul este = {0}", h);
 
-int gt = 5 + 15 / 3 * 2 - 8 % 3;
+var gt = 5 + 15 / 3 * 2 - 8 % 3;
 Console.WriteLine("Rezultatul este = {0}", gt);
 
-string name = "John";
+var name = "John";
 Console.WriteLine("Numele este {0} si varsta lui este {1}",name, gt);
 
