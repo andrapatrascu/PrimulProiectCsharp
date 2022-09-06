a1663ddc2340d90eada5e1434240b111dcffc7bc
andrapatrascu <patrascuandra@yahoo.com>
2022-09-06 11:16:38 +0300

    Revert "change to var"
    This reverts commit a69e0ac31cd1db19267be7d127b901f49e557282.

--- Program.cs
+++ Program.cs
@@ -2,42 +2,42 @@
 Console.WriteLine("Hello");
 Console.WriteLine("Andra");
 
-var x = 5;      // Am declarat variabila de tip int si i-am asignat valoarea 5
-var y = 101;
-var suma = x + y;
-var gigi = "ana are mere";
+int x = 5;      // Am declarat variabila de tip int si i-am asignat valoarea 5
+int y = 101;
+int suma = x + y;
+string gigi = "ana are mere";
 Console.WriteLine("suma dintre {0} si {1} este {2} si totodata {3}", x, y, suma, gigi);
 
-var z = 5.4;
-var t = 10100.56;
-var a_doua_suma = z+t;
+double z = 5.4;
+double t = 10100.56;
+double a_doua_suma = z+t;
 Console.WriteLine(a_doua_suma);
 
-var c = 5.4;
-var d = 2.1;
-var impartire = c/d;
+double c = 5.4;
+double d = 2.1;
+double impartire = c/d;
 Console.WriteLine("Impartirea dintre {0} si {1} este {2:0.000}", c, d,impartire);
 
-var p = -5 + 8 * 6;     // Am declarat variabila de tip int p si am initializat-o cu o expresie
+int p = -5 + 8 * 6;     // Am declarat variabila de tip int p si am initializat-o cu o expresie
 Console.WriteLine("Rezultat = {0}", p); 
 
 
 x = -5;     // Am asignat valoarea -5 variabilei x
 y = 8;
-var tx = 6;
-var rezultat = x + y * tx;
+int tx = 6;
+int rezultat = x + y * tx;
 Console.WriteLine(rezultat);
 
 
-var l = (55 + 9) % 9;
+int l = (55 + 9) % 9;
 Console.WriteLine("Rezultatul este = {0}", l);      // Afisez valoarea variabilei l
 
-var h = 20 + -3 * 5 / 8; 
+int h = 20 + -3 * 5 / 8; 
 Console.WriteLine("Rezultatul este = {0}", h);
 
-var gt = 5 + 15 / 3 * 2 - 8 % 3;
+int gt = 5 + 15 / 3 * 2 - 8 % 3;
 Console.WriteLine("Rezultatul este = {0}", gt);
 
-var name = "John";
+string name = "John";
 Console.WriteLine("Numele este {0} si varsta lui este {1}",name, gt);
 
