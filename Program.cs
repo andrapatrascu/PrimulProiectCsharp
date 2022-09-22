81fa0745cf8d0e2753981651047118588958f960
andrapatrascu <patrascuandra@yahoo.com>
2022-09-22 10:50:16 +0300

    Added examples for each operator.

--- Program.cs
+++ Program.cs
@@ -100,4 +100,172 @@ Console.WriteLine(xza + ytd);
 
 //"Operators" chapter
 
+// Basic assignment operator
 
+double ty;
+ty = 100.20; // 100.20 is assign to ty
+Console.WriteLine(ty);
+Console.WriteLine("The number is {0}",ty);
+
+int Number1, Number2;
+Number1 = 25;
+Number2 = 30;
+Console.WriteLine(Number1);
+Console.WriteLine("Number1 is {0} and Number2 is {1}", Number1,Number2);
+
+double Number3, Number4;
+Number3 = 50.50;
+Number4 = Number3;
+Console.WriteLine("Number4 is {0}", Number4);
+
+//Arithmetic operators
+
+double Number5 = 10.20D, Number6 = 45.87D , Result;
+Result = Number5 + Number6; // addition operation
+Console.WriteLine("{0} + {1} = {2}", Number5, Number6, Result);
+
+double Number7 = 10.20, Number8 = 0.5, Result1;
+Result1 = Number7 - Number8; // substraction operation
+Console.WriteLine("{0} - {1} = {2}", Number7, Number8, Result1);
+
+double Number9 = 10.20, Number10 = 0.5, Result2;
+Result2= Number9 * Number10; // mutiplication operation
+Console.WriteLine("{0} * {1} = {2}", Number9, Number10, Result2);
+
+double Number11 = 10.20, Number12 = 0.5, Result3;
+Result3= Number11 / Number12; // division operation
+Console.WriteLine("{0} / {1} = {2}", Number11, Number12, Result3);
+
+//Relational operators
+
+bool Result4;
+int FirstNumber1 = 10, SecondNumber1 = 20;
+Result4 = FirstNumber1 == SecondNumber1;// equal to
+Console.WriteLine("{0} == {1} returns {2}", FirstNumber1, SecondNumber1, Result4);
+
+bool Result5;
+int FirstNumber2 = 10, SecondNumber2 = 20;
+Result5 = FirstNumber2 > SecondNumber2;// greater than
+Console.WriteLine("{0} > {1} returns {2}", FirstNumber2, SecondNumber2, Result5);
+
+bool Result6;
+int FirstNumber3 = 10, SecondNumber3 = 20;
+Result6 = FirstNumber3 < SecondNumber3;// less than
+Console.WriteLine("{0} < {1} returns {2}", FirstNumber3, SecondNumber3, Result6);
+
+bool Result7;
+int FirstNumber4 = 10, SecondNumber4 = 20;
+Result7 = FirstNumber4 >= SecondNumber4;// greater than
+Console.WriteLine("{0} >= {1} returns {2}", FirstNumber4, SecondNumber4, Result7);
+
+bool Result8;
+int FirstNumber5= 10, SecondNumber5 = 20;
+Result8= FirstNumber5 <= SecondNumber5;// less than
+Console.WriteLine("{0} <= {1} returns {2}", FirstNumber5, SecondNumber5, Result8);
+
+bool Result9;
+int FirstNumber6 = 10, SecondNumber6 = 20;
+Result9 = FirstNumber5 != SecondNumber5;// not equal to
+Console.WriteLine("{0} != {1} returns {2}", FirstNumber6, SecondNumber6, Result9);
+
+// Logical operators
+
+bool Result10;
+int Number13 = 10, Number14 = 20;
+Result10 = (Number13 == Number14) || (Number13 > 5); // using OR operator
+Console.WriteLine(Result10);
+
+bool Result11;
+int Number15 = 10, Number16 = 20;
+Result11 = (Number15 == Number16) && (Number15 > 5); // using AND operator
+Console.WriteLine(Result11);
+
+//Unary operators
+
+int Number17 = 10, Result12;
+bool flag = true;
+Result12 = +Number17; // unary plus
+Console.WriteLine("+Number17 = " + Result12);
+
+int Number18 = 10, Result13;
+bool flag1 = true;
+Result13 = -Number18; // unary minus
+Console.WriteLine("-Number18 = " + Result13);
+
+int Number19= 10, Result14;
+bool flag2 = true;
+Result14 = ++Number19; // increment
+Console.WriteLine("++Number18 = " + Result14);
+
+int Number20 = 10, Result15;
+bool flag3 = true;
+Result15 = --Number20; // decrement
+Console.WriteLine("--Number18 = " + Result15);
+
+
+int Number21 = 10;
+Console.WriteLine((Number21++)); // posfix
+Console.WriteLine((Number21));
+
+int Number22 = 20;
+Console.WriteLine((++Number22)); // prefix
+Console.WriteLine((Number22));
+
+//Ternary operator
+
+int Number23 = 10;
+string Result16;
+Result16 = (Number23 / 2 == 0) ? "Even Number23" : "Odd Number"; // the result is "Odd Number"
+Console.WriteLine("{0} is {1}", Number23, Result16);
+
+int Number24 = 10;
+string Result17;
+Result17 = (Number24 / 2 >= 0) ? "Even Number24" : "Odd Number"; // the result is "Even Number24"
+Console.WriteLine("{0} is {1}", Number24, Result17);
+
+//Compound Assignment Operators
+
+int Number25 = 10;
+Number25 += 5;
+Console.WriteLine(Number25);
+
+int Number26 = 10;
+Number26 -= 3;
+Console.WriteLine(Number26);
+
+int Number27 = 10;
+Number27 *= 2;
+Console.WriteLine(Number27);
+
+int Number28 = 10;
+Number28 /= 3;
+Console.WriteLine(Number28);
+
+
+//Operator precedence-if two operators with different precedence are used, the operator with higher precedence is evaluated first.
+
+int xX = 4 + 3 * 5;
+Console.WriteLine("Result is {0}", xX);
+
+int Result18;
+int A = 5, B = 6, C = 4;
+Result18 = --A * B - ++C;
+Console.WriteLine(Result18);
+
+bool Result19;
+Result19 = B >= C + A;
+Console.WriteLine(Result19);
+
+//Associativity of Operators-if both the operators have same precedence,the expression is evaluated based on the associativity of operator (left to right or right to left).
+
+int D = 5, E = 6, F = 3;
+int Result20 = D * E/ F;
+Console.WriteLine(Result20);
+
+int G = 5, H = 6, I = 3;
+int Result21= G = H = I; //The associativity of = operator is from right to left
+Console.WriteLine(Result21);
+
+int J = 5, K = 6, L = 3;
+int Result23 = J = K = L;
+Console.WriteLine("J = {0}, K = {1}, L = {2}", J, K, L);
