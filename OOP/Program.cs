ee6e52ba7b6e4b79f55708cc2f68fb2a6bdb2247
andrapatrascu <patrascuandra@yahoo.com>
2022-10-27 12:17:36 +0300

    Created Classes, added Objects and Methods examples

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create Dog object
+            Dog bullDog = new Dog();
+
+            // access breed of the Dog 
+            bullDog.Breed = "Bull Dog";
+            Console.WriteLine(bullDog.Breed);// AFISEZ
+
+            // access method of the Dog
+            bullDog.Bark();
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+            Console.WriteLine(myObj1.Color);
+            Console.WriteLine(myObj2.Color);
+
+            Car myObj = new Car();
+            myObj.MaxSpeed = 200;
+            Console.WriteLine(myObj.Color);
+            Console.WriteLine(myObj.MaxSpeed);
+
+        }
+    }
+}
+
