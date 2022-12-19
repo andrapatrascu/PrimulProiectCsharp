23b2b1b5ee0e62eb74b7fe972c9e769bf5bc79eb
andrapatrascu <51133187+andrapatrascu@users.noreply.github.com>
2022-12-19 09:24:59 +0200

    Added examples for methods and classes (#28)
    * Added Person and Calculator classes and using them
    
    * Added changes to Client and Person classes

---
+++ OOP/Models/Calculator.cs
@@ -0,0 +1,46 @@
+﻿
+namespace OOP.Models
+{
+    internal class Calculator
+    {
+        private int _NumA;
+        private int _NumB;
+        private int _NumC;
+        private int _NumD;
+
+        public Calculator( int NumA, int NumB, int NumC, int NumD)
+        {
+            _NumA = NumA;
+            _NumB = NumB;
+            _NumC = NumC;
+            _NumD = NumD;
+        }
+
+        public void Add(out int Result)
+        {
+            Result = _NumA + _NumB + _NumC + _NumD;
+        }
+
+        public void Subtract(out int Result)
+        {
+            Result = _NumA - _NumB -_NumC - _NumD;
+        }
+
+        public void Multiply(out int Result)
+        {
+            Result = _NumA * _NumB * _NumC * _NumD;
+        }
+
+        public bool Division(out int Result)
+        {
+            if (_NumB == 0 || _NumC == 0 || _NumD == 0)
+            {
+                Result = 0;
+                return false;
+            }
+
+            Result = _NumA / _NumB / _NumC /_NumD;
+            return true;
+        }
+    }
+}
---
+++ OOP/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿
+namespace OOP.Models
+{
+    internal class Person
+    {
+       private string _name;
+
+        public Person(string Name)
+        {
+            _name = Name;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
